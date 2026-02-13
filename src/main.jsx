4b7957ba2bc715f702afb29f1eefa5e4fe632da7
Louis Hamilton <25408909+SillyRecon@users.noreply.github.com>
2026-02-13 17:46:16 -0600

    build: added bootstrap.bundle.min.js for animation

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router.jsx';
 import "boostrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 import './styles/index.css';
 
 
